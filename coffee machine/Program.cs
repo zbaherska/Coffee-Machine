76c155f0ebdbdb768d2574260d6a7b3d4b04d447
mka <mka@A34PC12>
2025-03-14 14:48:18 +0200

    Add menu

--- coffee machine/Program.cs
+++ coffee machine/Program.cs
@@ -1,3 +1,20 @@
 ﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+  Автомат гарячих напоїв. 
+Створити ієрархію класів для подання гарячих напоїв Автомату(кава, чай, капучіно та ін.)
+Створити програму для роботи Автомату у  режимах адмін та користувач
+У режимі адміна передбачити 	
+	Завантаження автомату водою, кава, чай, цукор
+	Вивід статистики наявності складових для приготування напоїв
+	Зміна цін на напої
+	Вилучення кешу
+У режимі користувача передбачити 		
+Замовлення напою(+ оплата)	
+ */
+Console.WriteLine("----------- Welcome -----------");
+
+Console.WriteLine("\tMenu:");
+Console.WriteLine("1. Load components");
+Console.WriteLine("2. Display statistics on the availability of ingredients");
+Console.WriteLine("3. Change drink prices");
+Console.WriteLine("4. Withdraw cash");
